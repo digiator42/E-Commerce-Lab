203393c99ff9f448b9da62d92eb101463ceeb682
Ahmed Hassan <digiator2020@gmail.com>
2026-03-28 03:36:58 +0200

    fix prd

--- src/main/java/com/ecommerce/lab/config/PostgresConfig.java
+++ src/main/java/com/ecommerce/lab/config/PostgresConfig.java
@@ -38,8 +38,8 @@ public class PostgresConfig {
         em.setJpaVendorAdapter(vendorAdapter);
 
         HashMap<String, Object> properties = new HashMap<>();
-        properties.put("hibernate.hbm2ddl.auto", "create-drop");
-        // properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         em.setJpaPropertyMap(properties);
 
         return em;
