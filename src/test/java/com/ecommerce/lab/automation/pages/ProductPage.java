b36b088421d0c372a0415fb58b621b0b736ca4eb
Ahmed Hassan <digiator2020@gmail.com>
2026-04-14 13:57:44 +0200

    fix: stale element

--- src/test/java/com/ecommerce/lab/automation/pages/ProductPage.java
+++ src/test/java/com/ecommerce/lab/automation/pages/ProductPage.java
@@ -1,11 +1,14 @@
 package com.ecommerce.lab.automation.pages;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.function.Predicate;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.ecommerce.lab.automation.components.Product;
 import com.ecommerce.lab.automation.utils.SeleniumUtils;
@@ -24,6 +27,10 @@ public class ProductPage {
     }
 
     public List<Product> getProducts() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlContains("/products"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("product-list-container")));
+
         List<WebElement> productElements = SeleniumUtils.waitForAllElements(
             driver,
             By.cssSelector("#product-list-container .group"),
